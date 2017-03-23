2998732a711a903dfc3153087a0df2465f73be27
vagrant <mayankaupadhyay@gmail.com>
2017-03-23 07:44:15 +0000

    added dependency

--- main.js
+++
@@ -1,9 +0,0 @@
-//  @Mayanka:  just require 'spellChecker' in your file to use it
-let spellcheck = require('./src/Domain-based-spellChecker');
-//  @Mayanka: sample wrong word or sentence
-let wrongWord = 'travl by bs';
-//  @Mayanka: passing it to spellcheck and storing correct word in correctSpell
-let correctSpell = spellcheck(wrongWord);
-console.log('Wrong word     : '+wrongWord);
-// @Mayanka:  query contains corrected word
-console.log('Corrected word : '+correctSpell.query);
